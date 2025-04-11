3427a1e5d204522edecc2e2e7c6a21a31e3d265d
myeongjooseo <juux_x@naver.com>
2025-04-11 11:29:46 +0900

    feat : entity 생성

---
+++ src/main/java/org/study/rankingsystem/domain/GameRecordLogs.java
@@ -0,0 +1,32 @@
+package org.study.rankingsystem.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "game_record_logs")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameRecordLogs {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	private int score;
+	private LocalDateTime playedAt;
+}
---
+++ src/test/java/org/study/rankingsystem/RankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.rankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

71551114d1ab493eeaea11ecc37356e426c68c24
myeongjooseo <juux_x@naver.com>
2025-04-11 11:28:19 +0900

    init : 프로젝트 생성

---
+++ src/main/java/org/study/rankingsystem/RankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.study.rankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingSystemApplication.class, args);
+	}
+
+}
